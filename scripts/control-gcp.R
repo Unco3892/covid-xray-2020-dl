85b13c44f6c8c85077206bf9e2ffbd0c6c746d3e
Unco3892 <47745523+Unco3892@users.noreply.github.com>
2020-05-14 16:03:43 +0200

    Adding the files for the tuning and training

---
+++ scripts/control-gcp.R
@@ -0,0 +1,24 @@
+library(keras)
+library(here)
+library(cloudml)
+
+# Placing all the data on the storage.
+gs_copy(
+    source = here::here("-"),
+    destination = "gs://-/",
+    recursive = TRUE
+)
+
+# Change the path after uplading the files
+setwd(here("scripts"))
+
+# File for the final re-training for learning rates
+cloudml_train(file="-",
+              master_type = "-", 
+              region = "-")
+
+# Setting the path for downloading the files
+setwd("runs")
+
+# Collecting the final re-train model and placing the model in the "results" folder
+job_collect("-")
---
+++ scripts/tune-CVOID-binary.R
@@ -0,0 +1,45 @@
+library(keras)
+library(tfruns)
+library(here)
+library(cloudml)
+library(tidyverse)
+library(kableExtra)
+
+
+# The tuning will take place on the cloud
+cloudml_train(file="tune-COVID-binary.R",
+              config = "tuning_binary_1.yml")
+
+# Setting the path for collecting the files
+setwd(here("runs"))
+
+# Collecting the final hyperparamter tuning
+job_collect("-", trials = "all")
+
+# Show the runs and sort them based on accuracy
+runs <- ls_runs(runs_dir = here("runs"))
+
+runs_ex2 <-
+    runs %>% filter(cloudml_job == "-") %>% select(flag_lr,
+                                                   metric_acc,
+                                                   metric_val_acc,
+                                                   metric_loss,
+                                                   metric_val_loss,
+                                                   completed) %>% arrange(desc(metric_val_acc)) %>% kable() %>% kable_styling(
+                                                       bootstrap_options = "striped",
+                                                       full_width = F,
+                                                       position = "center"
+)
+
+
+# ====================================
+# Only if needed to do a second tuning
+setwd(here("scripts"))
+
+# Only if needed to do a second tuning
+# cloudml_train(file="train-natural-lenet-5.R",
+#               config = "tuning_ex2_final_2.yml")
+
+# Collecting the job
+setwd(here("runs"))
+job_collect("-", trials= "all")
