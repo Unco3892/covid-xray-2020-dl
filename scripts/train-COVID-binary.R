f8ba4fd03c062142de8da36fbf64503a587345f3
schroeti <alexandre.schroeter@unil.ch>
2020-05-14 18:20:03 +0200

    Add train script

--- scripts/train-COVID-binary.R
+++ scripts/train-COVID-binary.R
@@ -1,6 +1,7 @@
 library(keras)
 library(cloudml)
 library(tfruns)
+library(here)
 
 # Define the hyperparameter for tuning
 # ------------------------------------
@@ -11,63 +12,62 @@ FLAGS <- flags(
 
 # Define the generator
 # ------------------------------------
-generator <- image_data_generator(rescale = 1 / 255, validation_split = 0.2)
+generator <- image_data_generator(rescale = 1 / 255, validation_split = 0.4, zoom_range = 0.2)
+
+
 
 
 # Import images
 # ------------------------------------
 train <- flow_images_from_directory(
-  directory = gs_data_dir_local(
-    "gs://deep-learning-274116/natural-images/train"
-  ),
-  target_size = c(100, 100),
+  directory = here("data/final_data"),
+  target_size = c(224, 224),
   generator = generator,
   batch_size = 8,
   subset = "training"
 )
 
 valid <- flow_images_from_directory(
-  directory = gs_data_dir_local(
-    "gs://deep-learning-274116/natural-images/train"
-  ),
-  target_size = c(100, 100),
+  directory = here("data/final_data"),
+  target_size = c(224, 224),
   generator = generator,
   batch_size = 8,
   subset = "validation"
 )
 
+
 #-----------------------------------------------------#
-#LeNet5 model architecture
+#DenseNet201 model architecture
+
+
+conv_base <- application_densenet201(include_top = FALSE, 
+                                     weights = "imagenet", 
+                                     input_shape = c(224, 224, 3))
+
+freeze_weights(conv_base)
 
 model <- keras_model_sequential() %>%
-  layer_conv_2d(filters = 6, kernel_size = c(5, 5),
-                strides = 1, padding = "same", # Note that we have used padding although it was not requested in the exercise but it was mentioned in the slides so it has been implemented.
-                activation = "relu",
-                input_shape = c(100, 100, 3)) %>% 
-  layer_average_pooling_2d(pool_size = c(2, 2), strides = 2) %>% 
-  layer_conv_2d(filters = 16, kernel_size = c(5, 5),
-                strides = 1, activation = "relu") %>% 
-  layer_average_pooling_2d(pool_size = c(2, 2), strides = 2) %>% 
-  layer_conv_2d(filters = 120, kernel_size = c(5, 5),
-                strides = 1, activation = "relu") %>% 
+  conv_base %>%
   layer_flatten() %>%
-  layer_dense(units = 84, activation = "relu") %>% 
-  layer_dense(units = 8, activation = "softmax")
+  layer_dense(units = 200, activation = "relu") %>%
+  layer_dense(units = 200, activation = "relu") %>%
+  layer_dense(units = 2, activation = "softmax")
 
-# Compile with the flags
+
 model %>% compile(
-  loss = "categorical_crossentropy",
-  optimizer = optimizer_rmsprop(FLAGS$lr),
+  optimizer = optimizer_rmsprop(lr = 0.0001),
+  loss = loss_categorical_crossentropy,
   metric = "accuracy"
-)    
+)
 
-# fit with the flags
 model %>% fit_generator(
-  generator = train, 
+  generator = train,
   steps_per_epoch = train$n / train$batch_size,
-  epochs = 100,
+  epochs = 10,
+  callbacks = callback_early_stopping(patience = 7,
+                                      restore_best_weights = TRUE),
   validation_data = valid,
-  validation_steps = valid$n / valid$batch_size,
-  callbacks = callback_early_stopping(patience = 5,
-                                      restore_best_weights = TRUE)
-)    
+  validation_steps = valid$n / valid$batch_size
+)
+
+model %>% evaluate_generator(generator = test, steps = test$n / test$batch_size)
