5be466da71a6cbbcc55b4278c4c8db56dd24a216
schroeti <alexandre.schroeter@unil.ch>
2020-05-16 15:18:27 +0200

    Update model

--- scripts/trial-train-COVID-binary.R
+++ scripts/trial-train-COVID-binary.R
@@ -1,60 +1,66 @@
-library(keras)
-library(cloudml)
-library(tfruns)
-library(here)
-
-# Define the generator
-# ------------------------------------
-generator <- image_data_generator(rescale = 1 / 255, validation_split = 0.2, zoom_range = 0.2)
-
-# Import images
-# ------------------------------------
-train <- flow_images_from_directory(
-  directory = here::here("data/final_data/train"),
-  target_size = c(224, 224),
-  generator = generator,
-  batch_size = 8,
-  subset = "training"
-)
-
-valid <- flow_images_from_directory(
-  directory = here::here("data/final_data/train"),
-  target_size = c(224, 224),
-  generator = generator,
-  batch_size = 8,
-  subset = "validation"
-)
-
-#-----------------------------------------------------#
-#DenseNet201 model architecture
-conv_base <- application_densenet201(
-  include_top = FALSE,
-  weights = "imagenet",
-  input_shape = c(224, 224, 3)
-)
-
-freeze_weights(conv_base)
-
-model <- keras_model_sequential() %>%
-  conv_base %>%
-  layer_flatten() %>%
-  layer_dense(units = 200, activation = "relu") %>%
-  layer_dense(units = 200, activation = "relu") %>%
-  layer_dense(units = 2, activation = "softmax")
-
-
-model %>% compile(
-  optimizer = optimizer_rmsprop(lr = 0.0001),
-  loss = loss_binary_crossentropy,
-  metric = "accuracy"
-)
-
-model %>% fit_generator(
-  generator = train,
-  steps_per_epoch = train$n / train$batch_size,
-  epochs = 2,
-  callbacks = callback_early_stopping(patience = 7,
-                                      restore_best_weights = TRUE),
-  validation_data = valid,
-  validation_steps = valid$n / valid$batch_size
-)
+  library(keras)
+  library(cloudml)
+  library(tfruns)
+  library(here)
+  
+  # Define the generator
+  # ------------------------------------
+  generator <- image_data_generator(rescale = 1 / 255, validation_split = 0.2, zoom_range = 0.2)
+  
+  # Import images
+  # ------------------------------------
+  train <- flow_images_from_directory(
+    directory = here::here("images/train"),
+    target_size = c(224, 224),
+    generator = generator,
+    batch_size = 16,
+    subset = "training"
+  )
+  
+  valid <- flow_images_from_directory(
+    directory = here::here("images/train"),
+    target_size = c(224, 224),
+    generator = generator,
+    batch_size = 16,
+    subset = "validation"
+  )
+  
+  #-----------------------------------------------------#
+  #DenseNet201 model architecture
+  conv_base <- application_densenet201(
+    include_top = FALSE,
+    weights = "imagenet",
+    input_shape = c(224, 224, 3)
+  )
+  
+  
+  conv_base <- application_vgg16(
+    include_top = FALSE,
+    weights = "imagenet",
+    input_shape = c(224, 224, 3)
+  )
+  freeze_weights(conv_base)
+  
+  model <- keras_model_sequential() %>%
+    conv_base %>%
+    layer_flatten() %>%
+    layer_dense(units = 100, activation = "relu") %>%
+    layer_dense(units = 100, activation = "relu") %>%
+    layer_dense(units = 4, activation = "softmax")
+  
+  
+  model %>% compile(
+    optimizer = optimizer_rmsprop(lr = 0.0001),
+    loss = loss_binary_crossentropy,
+    metric = "accuracy"
+  )
+  
+  model %>% fit_generator(
+    generator = train,
+    steps_per_epoch = train$n / train$batch_size,
+    epochs = 10,
+    callbacks = callback_early_stopping(patience = 7,
+                                        restore_best_weights = TRUE),
+    validation_data = valid,
+    validation_steps = valid$n / valid$batch_size
+  )
