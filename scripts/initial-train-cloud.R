b5fcd21d6bb0b513135b935be8c84fc4e1b87c17
Unco3892 <47745523+Unco3892@users.noreply.github.com>
2020-05-16 15:33:27 +0200

    Changes to the script

---
+++ scripts/initial-train-cloud.R
@@ -0,0 +1,13 @@
+library(keras)
+library(here)
+library(cloudml)
+
+
+# Training on the cloud
+cloudml_train(here::here("scripts/trial-train-COVID-binary.R"), master_type = "standard_p100")
+
+# Setting the path for downloading the files
+setwd("runs")
+
+# Collecting the final re-train model and placing the model in the "results" folder
+job_collect("-")
