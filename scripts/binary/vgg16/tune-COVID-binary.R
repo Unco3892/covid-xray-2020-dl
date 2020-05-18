3ba7ea28bcca1b3d073b8f74f90efd3d32912b62
Unco3892 <47745523+Unco3892@users.noreply.github.com>
2020-05-18 10:22:41 +0200

    Collected and analyzed binary vgg16 runs

--- scripts/binary/vgg16/tune-COVID-binary.R
+++ scripts/binary/vgg16/tune-COVID-binary.R
@@ -2,8 +2,8 @@ library(keras)
 library(tfruns)
 library(here)
 library(cloudml)
-# library(tidyverse)
-# library(kableExtra)
+library(tidyverse)
+library(kableExtra)
 
 # Change the working directory
 # setwd("tune-cloud")
@@ -14,28 +14,37 @@ cloudml_train(
   config = here::here("scripts/binary/vgg16/tuning_binary_1.yml")
 )
 
-# Setting the path for collecting the files
+# Setting the path for collecting the files vgg16 binary files
 setwd(here("runs/binary/vgg16"))
 getwd()
-
-# Collecting the final hyperparamter tuning
-job_collect("-", trials = "all")
+# Collecting the final hyperparamter tuning (this id refers to the hyperparameter tuning of vgg16 binary files)
+job_collect("cloudml_2020_05_17_172621619", trials = "all")
 
 # Show the runs and sort them based on accuracy
-runs <- ls_runs(runs_dir = here("scripts/runs/binary")) #it could also be categorical
+runs <- ls_runs(runs_dir = here::here("runs/binary/vgg16/runs")) #it could also be categorical
 
-runs_report <-
-  runs %>% filter(cloudml_job == "-") %>% select(flag_lr,
-                                                 metric_acc,
-                                                 metric_val_acc,
-                                                 metric_loss,
-                                                 metric_val_loss,
-                                                 completed) %>% arrange(desc(metric_val_acc)) %>% kable() %>% kable_styling(bootstrap_options = "striped",full_width = F,position = "center")
+View(runs)
+runs_report <- runs %>% select(metric_acc, metric_val_acc,metric_loss,metric_val_loss,flag_lr, epochs_completed, flag_dropoutrate,flag_reg, flag_units1, flag_units2, flag_optimizer,flag_activation, cloudml_state, run_dir) %>% arrange(desc(metric_val_acc), metric_val_loss) %>% kable() %>% kable_styling(bootstrap_options = "striped",full_width = F,position = "center")
 
-# Only if needed to do a second tuning
-# cloudml_train(file="train-natural-lenet-5.R",
-#               config = "tuning_ex2_final_2.yml")
+# Viewing the best runs
+view_run(here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-089")) # The first one drops to almost 40% at one point so it is not good
 
-# Collecting the job
-setwd(here("runs"))
-job_collect("-", trials = "all")
+# Comparing the second and third run
+compare_runs(
+  runs = c(
+    here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-059"), # should be modified
+  here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-057")  # should be modified
+  )
+) # The run ending with 057 drops to almost 40% at one point so it is not good and the second run (cloudml_2020_05_17_172621619-059) is definitly the best run so far, you can view the run below
+# view_run(here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-057")) 
+
+
+#  Comparing the fourth and the fifth run
+compare_runs(
+  runs = c(
+    here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-027"), # should be modified
+    here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-122")  # should be modified
+  )
+) # The run ending with 027 also seems to be good like the last one however the fifth like just like the third run seems to fall drastically and should be ignored.
+
+
