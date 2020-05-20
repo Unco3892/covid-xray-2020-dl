c1a5e4d4b91f6cd772b047780e30c08b1dcba993
Unco3892 <47745523+Unco3892@users.noreply.github.com>
2020-05-20 19:51:52 +0200

    Mistakes identified

--- scripts/binary/densenet201/tune-COVID-binary.R
+++ scripts/binary/densenet201/tune-COVID-binary.R
@@ -2,48 +2,58 @@ library(keras)
 library(tfruns)
 library(here)
 library(cloudml)
-# library(tidyverse)
-# library(kableExtra)
+library(tidyverse)
+library(kableExtra)
 
 # Change the working directory
 
 setwd(here::here("scripts/binary/densenet201/"))
 
 # The tuning will take place on the cloud
-cloudml_train(
-  file = "train-COVID-binary.R",
-  config = "tuning_binary_dense1.yml")
+cloudml_train(file = "train-COVID-binary.R",
+              config = "tuning_binary_dense1.yml")
 
 
 # Setting the path for collecting the files
-setwd(here("runs/binary/densenet201"))
+setwd(here::here("runs/binary/densenet201"))
 getwd()
 
-# Collecting the final hyperparamter tuning
-job_collect("-", trials = "all")
+# Collecting the final hyperparamter tuning for densenet201 binary
+job_collect("cloudml_2020_05_20_141743560", trials = "all")
 
 # Show the runs and sort them based on accuracy
-runs <- ls_runs(runs_dir = here("scripts/runs/binary")) #it could also be categorical
+runs <- ls_runs(runs_dir = here("runs/binary/densenet201/runs"))
 
 runs_report <-
-  runs %>% filter(cloudml_job == "-") %>% select(flag_lr,
-                                                 metric_acc,
-                                                 metric_val_acc,
-                                                 metric_loss,
-                                                 metric_val_loss,
-                                                 completed) %>% arrange(desc(metric_val_acc)) %>% kable() %>% kable_styling(bootstrap_options = "striped",full_width = F,position = "center")
+  runs %>% select(3, 5, 2, 4, 6:12, cloudml_state, run_dir) %>% arrange(desc(metric_val_categorical_accuracy), metric_val_loss) %>% kable() %>% kable_styling(
+    bootstrap_options = "striped",
+    full_width = F,
+    position = "center"
+  )
 
-# Only if needed to do a second tuning
-# cloudml_train(file="train-natural-lenet-5.R",
-#               config = "tuning_ex2_final_2.yml")
+view_run(here::here(
+  "runs/binary/densenet201/runs/cloudml_2020_05_20_141743560-080"
+)) # The first one is definitly not good as it drops very low.
 
-# Collecting the job
-setwd(here("runs"))
-job_collect("-", trials = "all")
+# Comparing the second and third run
+compare_runs(runs = c(
+  here::here(
+    "runs/binary/densenet201/runs/cloudml_2020_05_20_141743560-085"
+  ),
+  here::here(
+    "runs/binary/densenet201/runs/cloudml_2020_05_20_141743560-050"
+  )
+)) # The third run looks good
 
 
-install.packages("remotes")
-library(remotes)
-install_github("irudnyts/cloudml")
-library(cloudml)
-gcloud_install()
+# Comparing fourth and fifth run
+compare_runs(runs = c(
+  here::here(
+    "runs/binary/densenet201/runs/cloudml_2020_05_20_141743560-090"
+  ),
+  here::here(
+    "runs/binary/densenet201/runs/cloudml_2020_05_20_141743560-122"
+  )
+)) # Neither of these runs is good
+
+# CONCLUSION: TAKE THE 3RD RUN with id cloudml_2020_05_20_141743560-050
--- scripts/binary/vgg16/tune-COVID-binary.R
+++ scripts/binary/vgg16/tune-COVID-binary.R
@@ -18,37 +18,42 @@ cloudml_train(
 setwd(here("runs/binary/vgg16"))
 getwd()
 # Collecting the final hyperparamter tuning (this id refers to the hyperparameter tuning of vgg16 binary files)
-job_collect("cloudml_2020_05_18_145238086", trials = "all")
+# job_collect("cloudml_2020_05_18_145238086", trials = "all")
 
 # Show the runs and sort them based on accuracy
 runs <- ls_runs(runs_dir = here::here("runs/binary/vgg16/runs")) #it could also be categorical
+# View(runs)
 
-View(runs)
-runs_report <- runs %>% select(metric_acc, metric_val_acc,metric_loss,metric_val_loss,flag_lr, epochs_completed, flag_dropoutrate,flag_reg, flag_units1, flag_units2, flag_optimizer,flag_activation, cloudml_state, run_dir) %>% arrange(desc(metric_val_acc), metric_val_loss) %>% kable() %>% kable_styling(bootstrap_options = "striped",full_width = F,position = "center")
+runs_report <-
+  runs %>% select(3, 5, 2, 4, 6:12, cloudml_state, run_dir) %>% arrange(desc(metric_val_categorical_accuracy), metric_val_loss) %>% kable() %>% kable_styling(
+    bootstrap_options = "striped",
+    full_width = F,
+    position = "center"
+  )
 
-# Viewing the best runs
-view_run(here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-112")) # The first one drops to almost 40% at one point so it is not good
-view_run(here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-009"))
+# REQUIRES CHANGE AND CONTROL
 
+# Viewing the first run
+view_run(here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-106")) # The first drops one to 40% which is not so desirable
 
+# Viewing the second run
+view_run(here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-054")) # THIS IS A GREAT RUN
 
-# Comparing the second and third run
+# Comparing the third and foruth run
 compare_runs(
   runs = c(
-    here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-059"), # should be modified
-  here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-057")  # should be modified
+    here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-014"), # This run is also great but not as good as the second run
+    here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-126")  # Not a good run
   )
-) # The run ending with 057 drops to almost 40% at one point so it is not good and the second run (cloudml_2020_05_17_172621619-059) is definitly the best run so far, you can view the run below
-# view_run(here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-057")) 
+) # A competitor for the 2nd best run is the 3rd run and the only difference between the two is the optimizer and the learning rate.
 
-
-#  Comparing the fourth and the fifth run
+#  Comparing the fifth and the sixth run
 compare_runs(
   runs = c(
-    here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-027"), # should be modified
-    here::here("runs/binary/vgg16/runs/cloudml_2020_05_17_172621619-122")  # should be modified
+    here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-125"), # should be modified
+    here::here("runs/binary/vgg16/runs/cloudml_2020_05_18_145238086-117")  # should be modified
   )
-) # The run ending with 027 also seems to be good like the last one however the fifth like just like the third run seems to fall drastically and should be ignored.
+) # 6th run is much more stable because of the optimizer adamax
 
+# CONCLUSION: TAKE THE 2ND RUN AND THE 3RD ONE. USE ADAMAX OPTIMIZER
 
-è'
